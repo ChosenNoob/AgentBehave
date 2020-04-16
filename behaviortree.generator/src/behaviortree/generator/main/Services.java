994f8075858113d5da7d8c2aff4aa35bed345d78
Baran Karaaslan <barankaraaslan_06@hotmail.com>
2020-04-16 03:54:42 +0300

    Now we can use java to generate code

---
+++ behaviortree.generator/src/behaviortree/generator/main/Services.java
@@ -0,0 +1,11 @@
+package behaviortree.generator.main;
+
+import behaviortree.BehaviorTree;
+
+public class Services {
+	public String generate(BehaviorTree behaviortree){
+		String generatedCode;
+		generatedCode = "Hello World!";
+		return generatedCode;
+	}
+}
